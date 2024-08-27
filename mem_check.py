02fe24ebc48de5f415d16741834c52cca1375cf5
HGB <87762857+radna0@users.noreply.github.com>
2024-08-27 13:20:56 +0700

    Update mem_check.py

--- mem_check.py
+++ mem_check.py
@@ -11,10 +11,16 @@ total = {
 }
 for device in devices:
         mem = round(xm.get_memory_info(device)["bytes_limit"] / 1e9, 2)
+        total[1] += mem
+        print(f'Total TPU device: {device} memory: {mem} GB')
+ 
+print(f"Total TPU memory: {total[0]} / {total[1]} GB")
+
+for device in devices:
+        mem = round(xm.get_memory_info(device)["bytes_limit"] / 1e9, 2)
         t = torch.randn(torch.randint(1, 8, (1,)), 4, 144, 720, 1280).to(device)
         mem_used =  round(xm.get_memory_info(device)["bytes_used"] / 1e9, 2)
         total[0] += mem_used
-        total[1] += mem
         print(f'Total TPU device: {device} memory: {mem_used} / {mem} GB')
         xm.mark_step()
 
