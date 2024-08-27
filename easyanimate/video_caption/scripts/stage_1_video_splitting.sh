250a1e5f345fb54e97d617ba75222a7514ab2c36
HGB <87762857+radna0@users.noreply.github.com>
2024-08-28 00:53:58 +0700

    Update stage_1_video_splitting.sh

--- easyanimate/video_caption/scripts/stage_1_video_splitting.sh
+++ easyanimate/video_caption/scripts/stage_1_video_splitting.sh
@@ -11,14 +11,14 @@ export MAX_SECONDS=10
 # measure the duration to process
 export START_TIME=$(date +%s)
 # Save all video names in a video folder as a meta file.
-python -m utils.get_meta_file \
+python3.10 -m utils.get_meta_file \
     --video_folder $VIDEO_FOLDER \
     --saved_path $META_FILE_PATH
 
 # Perform scene detection on the video dataset.
 # Adjust the n_jobs parameter based on the actual number of CPU cores in the machine.
 export START_TIME1=$(date +%s)
-python cutscene_detect.py \
+python3.10 cutscene_detect.py \
     --video_metadata_path $META_FILE_PATH \
     --video_folder $VIDEO_FOLDER \
     --saved_folder $SCENE_FOLDER \
@@ -32,7 +32,7 @@ echo "Duration: $DURATION seconds"
 # Gather all scene jsonl files to a single scene jsonl file.
 # Adjust the n_jobs parameter based on the actual I/O speed in the machine.
 export START_TIME2=$(date +%s)
-python -m utils.gather_jsonl \
+python3.10 -m utils.gather_jsonl \
     --meta_folder $SCENE_FOLDER \
     --meta_file_path $SCENE_SAVED_PATH \
     --n_jobs 96
@@ -45,7 +45,7 @@ echo "Duration: $DURATION seconds"
 # Perform video splitting filtered by the RESOLUTION_THRESHOLD.
 # It consumes more CPU computing resources compared to the above operations.
 export START_TIME3=$(date +%s)
-python video_splitting.py \
+python3.10 video_splitting.py \
     --video_metadata_path $SCENE_SAVED_PATH \
     --video_folder $VIDEO_FOLDER \
     --output_folder $OUTPUT_FOLDER \
